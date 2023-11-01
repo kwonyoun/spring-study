6e0360a8afb92fc238f760e39ba6f6791609f82e
kwon <kyjyj6334@gmail.com>
2023-11-02 00:44:55 +0900

    toString 수정

--- src/main/java/hello/core/order/Order.java
+++ src/main/java/hello/core/order/Order.java
@@ -51,12 +51,12 @@ public class Order {
 
      @Override
      public String toString() {
-         return "Order{"+
-         "memberId= "+memberId+
-         ", itemName= "+itemName+
-         ", itemPrice= "+itemPrice+
-         ", discountPrice= "+discountPrice+
-         '}';
+        return "Order{" +
+            "memberId=" + memberId +
+            ", itemName='" + itemName + '\'' +
+            ", itemPrice=" + itemPrice +
+            ", discountPrice=" + discountPrice +
+            '}';
      }
 
 
